b926a5167222ac1f170bcd016f9f0b777f2229eb
Alexander Stoev <stoev95pd@gmail.com>
2022-02-10 18:13:43 +0200

    implemented the Layout and Providers components in order to provide a basic skeleton for the app

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,18 +1,22 @@
 import type { AppProps } from 'next/app';
 import Head from 'next/head';
+import Layout from 'src/components/atoms/Layout';
+import Providers from 'src/components/atoms/Providers';
 import '../styles/index.css';
 
 function App({ Component, pageProps }: AppProps) {
   return (
-    <>
+    <Providers>
       <Head>
         <title>MVP Match</title>
         <meta name="description" content="MVP Match assignment" />
         <link rel="icon" href="/favicon.ico" />
       </Head>
 
-      <Component {...pageProps} />
-    </>
+      <Layout>
+        <Component {...pageProps} />
+      </Layout>
+    </Providers>
   );
 }
 
