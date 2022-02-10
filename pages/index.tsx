65b4d6d773f93f4c814496980e7598f5347a2871
Alexander Stoev <stoev95pd@gmail.com>
2022-02-10 17:23:29 +0200

    extracted all colors from the Figma design and hooked them up to Tailwind

--- pages/index.tsx
+++ pages/index.tsx
@@ -3,7 +3,7 @@ import type { NextPage } from 'next';
 const Home: NextPage = () => {
   return (
     <div>
-      <div className="text-3xl">Hello from Tailwind</div>
+      <div className="text-3xl text-mvp-blue-200">Hello from Tailwind</div>
     </div>
   );
 };
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,7 +1,32 @@
 module.exports = {
   content: ['./pages/**/*.{js,ts,jsx,tsx}', './src/components/**/*.{js,ts,jsx,tsx}'],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        mvp: {
+          green: {
+            50: 'var(--mvp-green-50)',
+          },
+          blue: {
+            50: 'var(--mvp-blue-50)',
+            100: 'var(--mvp-blue-100)',
+            200: 'var(--mvp-blue-200)',
+            300: 'var(--mvp-blue-300)',
+            400: 'var(--mvp-blue-400)',
+          },
+          yellow: {
+            50: 'var(--mvp-yellow-50)',
+            100: 'var(--mvp-yellow-100)',
+          },
+          red: {
+            50: 'var(--mvp-red-50)',
+          },
+          purple: {
+            50: 'var(--mvp-purple-50)',
+          },
+        },
+      },
+    },
   },
   plugins: [],
 };
