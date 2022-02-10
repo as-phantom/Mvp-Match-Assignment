641b14585e2861be3a34f5a71c45bfcb1c1fb8f9
Alexander Stoev <stoev95pd@gmail.com>
2022-02-10 18:48:21 +0200

    implemented the Footer component

---
+++ src/components/organisms/common/Footer.tsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Footer: React.FC = () => {
+  return (
+    <footer className="py-6">
+      <p className="text-mvp-blue-300 font-bold">Terms &amp; Conditions | Privacy policy</p>
+    </footer>
+  );
+};
+
+export default Footer;
