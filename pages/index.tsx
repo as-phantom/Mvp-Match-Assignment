7429c1059aa4d2f56fa4120376a72a3e49f736c4
Alexander Stoev <stoev95pd@gmail.com>
2022-02-10 17:58:59 +0200

    extracted common classes and styles to be reused across the project

--- pages/index.tsx
+++ pages/index.tsx
@@ -3,7 +3,20 @@ import type { NextPage } from 'next';
 const Home: NextPage = () => {
   return (
     <div>
-      <div className="text-3xl text-mvp-blue-200">Hello from Tailwind</div>
+      <h3 className="text-mvp-blue-200 text-3xl">Hello from Tailwind</h3>
+
+      <button type="button" className="btn btn-0">
+        All projects
+      </button>
+
+      <button type="button" className="btn btn-1">
+        Generate report
+      </button>
+
+      <p>
+        Lorem ipsum dolor sit amet consectetur, adipisicing elit. Reprehenderit fugit, illum iure sit a laudantium accusamus delectus ut
+        quidem dicta. Doloribus distinctio consequuntur, praesentium repellat voluptate laboriosam! Ullam, consequatur. Est.
+      </p>
     </div>
   );
 };
