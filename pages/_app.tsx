88d3441a8adb6589734c741853c74b4573ccb3ab
Alexander Stoev <stoev95pd@gmail.com>
2022-02-10 17:35:58 +0200

    extracted the assignment logo from the Figma design and went ahead and made an .ico version of it in order to use it as a favicon

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,8 +1,19 @@
 import type { AppProps } from 'next/app';
+import Head from 'next/head';
 import '../styles/index.css';
 
 function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />;
+  return (
+    <>
+      <Head>
+        <title>MVP Match</title>
+        <meta name="description" content="MVP Match assignment" />
+        <link rel="icon" href="/favicon.ico" />
+      </Head>
+
+      <Component {...pageProps} />
+    </>
+  );
 }
 
 export default App;
