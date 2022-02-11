f5693f84dfc43572b0719d74b1521b6a5d372b0f
Alexander Stoev <stoev95pd@gmail.com>
2022-02-11 16:28:06 +0200

    some further work on the pie chart

--- data/HomePage/pieChart.ts
+++ data/HomePage/pieChart.ts
@@ -1,22 +1,54 @@
+import { localizeAmount } from 'lib/localizeAmount';
 import { IGateway } from 'src/interfaces/gateway.interface';
 import { IProject } from 'src/interfaces/project.interface';
 import { IReport } from 'src/interfaces/report.interface';
 
-export function pieChartOption(reports: IReport[], currentProject: IProject | undefined, currentGateway: IGateway | undefined) {
+export function pieChartOption(
+  reports: IReport[],
+  projects: IProject[],
+  gateways: IGateway[],
+  currentProject: IProject | undefined,
+  currentGateway: IGateway | undefined
+) {
+  let data: { value: number; name: string }[] = [];
+
   if (currentProject && !currentGateway) {
-    reports = reports.filter((r) => r.projectId === currentProject.projectId);
+    const reportsByProjectId = reports.filter((r) => r.projectId === currentProject.projectId);
+
+    data = gateways.map(({ gatewayId, name }) => {
+      const value = reportsByProjectId.filter((r) => r.gatewayId === gatewayId).reduce((a, c) => a + c.amount, 0);
+
+      return { value, name };
+    });
   } else if (!currentProject && currentGateway) {
-    reports = reports.filter((r) => r.gatewayId === currentGateway.gatewayId);
+    const reportsByGatewayId = reports.filter((r) => r.gatewayId === currentGateway.gatewayId);
+
+    data = projects.map(({ projectId, name }) => {
+      const value = reportsByGatewayId.filter((r) => r.projectId === projectId).reduce((a, c) => a + c.amount, 0);
+
+      return { value, name: name };
+    });
   }
 
-  const total: number = reports.reduce((a, c) => a + c.amount, 0);
-  const data = reports.map((r) => ({ value: r.amount, name: `${((r.amount / total) * 100).toFixed(1)}%` }));
-
   return {
+    tooltip: {
+      show: true,
+      formatter: ({ value, name }: { value: number; name: string }) =>
+        `<strong style="color: #011f4b">${name}: ${localizeAmount(value)}</strong>`,
+    },
+    legend: {
+      show: true,
+      padding: 16,
+      borderRadius: 8,
+      backgroundColor: '#f1fafe',
+      left: 0,
+      z: 0,
+    },
     series: [
       {
         type: 'pie',
-        radius: ['40%', '70%'],
+        radius: ['30%', '65%'],
+        center: ['50%', '57.5%'],
         legend: {
           show: true,
         },
@@ -24,6 +56,7 @@ export function pieChartOption(reports: IReport[], currentProject: IProject | un
           show: true,
           position: 'inside',
           color: 'white',
+          formatter: ({ percent }: { percent: number }) => `${percent}%`,
         },
         data,
       },
