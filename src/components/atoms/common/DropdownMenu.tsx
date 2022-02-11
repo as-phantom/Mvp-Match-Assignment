1abc7c8514b3fc8894a2cd3f89af7ac19aad9bab
Alexander Stoev <stoev95pd@gmail.com>
2022-02-11 16:28:37 +0200

    applied z index 1 to the DropdownMenu as it was getting hidden by the chart legend

--- src/components/atoms/common/DropdownMenu.tsx
+++ src/components/atoms/common/DropdownMenu.tsx
@@ -2,7 +2,7 @@ import React from 'react';
 
 const DropdownMenu: React.FC = ({ children }) => {
   return (
-    <ul className="bg-mvp-green-50 rounded-0 no-scrollbar invisible absolute left-0 right-0 top-full max-h-56 overflow-auto pb-2 opacity-0 transition-all group-focus:visible group-focus:rounded-tl-none group-focus:rounded-tr-none group-focus:opacity-100">
+    <ul className="bg-mvp-green-50 rounded-0 no-scrollbar invisible absolute left-0 right-0 top-full z-[1] max-h-56 overflow-auto pb-2 opacity-0 transition-all group-focus:visible group-focus:rounded-tl-none group-focus:rounded-tr-none group-focus:opacity-100">
       {children}
     </ul>
   );
